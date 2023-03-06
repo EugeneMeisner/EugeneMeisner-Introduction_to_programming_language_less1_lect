858d7dcdc3ddceb88dffd5398d00f97607f5193a
Eugene <reitelx2@gmail.com>
2023-03-07 00:10:35 +0300

    Condition If Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Enter you name:  ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "masha")
+{
+    Console.WriteLine("Wow it's Masha");
+}
+else
+{
+    Console.WriteLine("Hi ");
+    Console.Write(username);
+}
