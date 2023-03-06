11984392cbb4d55c406eeabfb2203214c50bb7a5
Eugene <reitelx2@gmail.com>
2023-03-06 23:46:45 +0300

    random

--- Example003_sum/Program.cs
+++ Example003_sum/Program.cs
@@ -1,7 +1,10 @@
-﻿int numberA = 3;
+﻿int numberA = new Random().Next(1, 10); // 1,2,3,4.....9
+Console.Write("Number 1 = ");
+Console.WriteLine(numberA);
 
-int numberB = 5;
+int numberB = new Random().Next(1, 10);
+Console.Write("Number 2 = ");
+Console.WriteLine(numberB);
 
-int result = numberA + numberB;
-
-Console.WriteLine(result);
+Console.Write("Result = ");
+Console.WriteLine(numberA + numberB);
